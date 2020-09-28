731b55c407ad74f96d19a56c735abc94cacc266a
wakuto <waku.902002@gmail.com>
2020-09-28 21:52:11 +0900

    01_bpb

--- src/01_bpb/boot.s
+++ src/01_bpb/boot.s
@@ -1,5 +1,12 @@
 entry:
-    jmp $       ; while(1);
+    jmp ipl      ; jump to ipl
+
+    ; BPB(BIOS Parameter Block
+    times 90 - ($ - $$) db 0x90
+
+    ; IPL(Initial Program Loader)
+ipl:
+    jmp $       ; while(1)
 
     times 510 - ($ - $$) db 0x00
     db 0x55, 0xAA
